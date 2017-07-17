6774dd1459998b1d2ae9afee5a7cd9d92a011191
Midhet <djulovic_m@hotmail.com>
2017-07-17 17:28:04 +0200

    Add Meteor method and tests for notes.update

--- imports/api/notes.test.js
+++ imports/api/notes.test.js
@@ -5,6 +5,20 @@ import { Notes } from './notes';
 
 if (Meteor.isServer) {
     describe('notes', function () {
+        const noteOne = {
+            _id: 'testNoteId1',
+            title: 'My title',
+            body: "My body for note",
+            updatedAt: 0,
+            userId: 'testUserId1'
+        };
+
+        beforeEach(function () {
+            Notes.remove({});
+            Notes.insert(noteOne)
+        });
+
+
         it('should insert new note', function () {
             const userId = 'dklajvc3';
             const _id = Meteor.server.method_handlers['notes.insert'].apply({ userId });
@@ -19,5 +33,45 @@ if (Meteor.isServer) {
 
             }).toThrow();
         })
+
+        it('should remove note', function () {
+            Meteor.server.method_handlers['notes.remove'].apply({ userId: noteOne.userId }, [noteOne._id])
+            expect(Notes.findOne({ _id: noteOne._id })).toNotExist();
+        });
+
+        it('should not remove note if unauthenticated', function () {
+            expect(() => {
+                Meteor.server.method_handlers['notes.remove'].apply({}, [noteOne._id])
+            }).toThrow();
+        });
+
+        it('should not remove note if invalid _id', function () {
+            expect(() => {
+                Meteor.server.method_handlers['notes.remove'].apply({ userId: noteOne.userId });
+            }).toThrow();
+        })
+
+        it('should update note ', function () {
+            const title = 'This is an updated title';
+
+           Meteor.server.method_handlers['notes.update'].apply({
+                userId: noteOne.userId
+            }, [
+                    noteOne._id,
+                    { title }
+                ])
+            const note = Notes.findOne(noteOne._id);
+            expect(note.updatedAt).toBeGreaterThan(0);
+            expect(note).toInclude({
+                title,
+                body: noteOne.body
+            })
+        });
+
+        it('should throw error if extra updates', function(){
+          expect(()=>{
+          Meteor.server.method_handlers['notes.update'].apply({userId:noteOne.userId}, [{newProp:"newProp"}])
+          }).toThrow();
+        });
     });
 }
