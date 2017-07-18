73bb383b841308350feca3d382503d4a7c77d589
Midhet <djulovic_m@hotmail.com>
2017-07-18 10:36:31 +0200

    Add PrivateHeader handleLogout test

--- imports/ui/PrivateHeader.test.js
+++ imports/ui/PrivateHeader.test.js
@@ -8,18 +8,25 @@ import PrivateHeader from './PrivateHeader';
 if (Meteor.isClient) {
     describe('PrivateHeader', function () {
         it('should set button text to logout', function () {
-            const wrapper = mount(<PrivateHeader title="test title" />);
+            const wrapper = mount(<PrivateHeader title="test title" handleLogout={() => { }} />);
             const buttonText = wrapper.find('button').text();
 
             expect(buttonText).toBe('Logout');
         })
 
-        it('should use title props as h1 text', function(){
-            const title="new test title";
+        it('should use title props as h1 text', function () {
+            const title = "new test title";
 
-            const wrapper = mount(<PrivateHeader title={title} />);
-            const h1Text= wrapper.find('h1').text();
+            const wrapper = mount(<PrivateHeader title={title} handleLogout={() => { }} />);
+            const h1Text = wrapper.find('h1').text();
             expect(h1Text).toBe(title);
+        })
+
+        it('should call handleLogout on click', function () {
+            const spy = expect.createSpy();
+            const wrapper = mount(<PrivateHeader title="test" handleLogout={spy} />);
+            wrapper.find('button').simulate('click');
+            expect(spy).toHaveBeenCalled();
         })
     });
 }
