cb0045325168807986bb5f196b3cd04e0f9243e7
Midhet <djulovic_m@hotmail.com>
2017-07-20 19:55:19 +0200

    Route seleted note to url

--- client/main.js
+++ client/main.js
@@ -4,6 +4,7 @@ import { Tracker } from "meteor/tracker";
 import { routes, onAuthChange } from '../imports/routes/routes';
 import  '../imports/startup/simple-schema-configuration';
 import {Session} from 'meteor/session';
+import {browserHistory} from 'react-router';
 
 Tracker.autorun(() => {
   const isAuthenticated = !!Meteor.userId();
@@ -11,8 +12,14 @@ Tracker.autorun(() => {
 
 });
 
+Tracker.autorun(()=>{
+const selectedNoteId=Session.get('selectedNoteId');
+if(selectedNoteId){
+ browserHistory.replace(`/dashboard/${selectedNoteId}`)
+}
+});
 
 Meteor.startup(() => {
-  
+   Session.set('selectedNoteId', undefined); 
   ReactDOM.render(routes, document.getElementById("app"));
 });
