d241d5243e264575973d2c93b3344d1b863f6ed1
Midhet <djulovic_m@hotmail.com>
2017-07-20 19:40:22 +0200

    Render empty item if no notes

--- imports/ui/NoteList.js
+++ imports/ui/NoteList.js
@@ -5,11 +5,12 @@ import { Notes } from '../api/notes';
 import PropTypes from 'prop-types';
 import NoteListHeader from './NoteListHeader';
 import NoteListItem from './NoteListItem';
-
+import NoteListEmptyItem from './NoteListEmptyItem'
 export const NoteList = (props) => {
     return (
         <div>
             <NoteListHeader />
+            {props.notes.length===0 ? <NoteListEmptyItem /> : undefined}
             {props.notes.map((note)=> <NoteListItem key={note._id} note={note} />)}
             NoteList {props.notes.length}
         </div>
