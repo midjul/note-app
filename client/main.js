16225fa21b735e411c61c1aa09bf60e587966a67
Midhet <djulovic_m@hotmail.com>
2017-07-22 17:32:55 +0200

    Refactor router to allow for dynamic urls

--- client/main.js
+++ client/main.js
@@ -2,24 +2,26 @@ import { Meteor } from "meteor/meteor";
 import ReactDOM from "react-dom";
 import { Tracker } from "meteor/tracker";
 import { routes, onAuthChange } from '../imports/routes/routes';
-import  '../imports/startup/simple-schema-configuration';
-import {Session} from 'meteor/session';
-import {browserHistory} from 'react-router';
+import '../imports/startup/simple-schema-configuration';
+import { Session } from 'meteor/session';
+import { browserHistory } from 'react-router';
 
 Tracker.autorun(() => {
   const isAuthenticated = !!Meteor.userId();
-  onAuthChange(isAuthenticated);
+  const currentPagePrivacy = Session.get('currentPagePrivacy');
+  onAuthChange(isAuthenticated, currentPagePrivacy);
+
 
 });
 
-Tracker.autorun(()=>{
-const selectedNoteId=Session.get('selectedNoteId');
-if(selectedNoteId){
- browserHistory.replace(`/dashboard/${selectedNoteId}`)
-}
+Tracker.autorun(() => {
+  const selectedNoteId = Session.get('selectedNoteId');
+  if (selectedNoteId) {
+    browserHistory.replace(`/dashboard/${selectedNoteId}`)
+  }
 });
 
 Meteor.startup(() => {
-   Session.set('selectedNoteId', undefined); 
+  Session.set('selectedNoteId', undefined);
   ReactDOM.render(routes, document.getElementById("app"));
 });
