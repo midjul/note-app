e52ef180b3117403a8d1c9b661707e06e6aad48e
Midhet <djulovic_m@hotmail.com>
2017-07-27 13:16:46 +0200

    Switch bars/x icon with session variable

--- imports/ui/PrivateHeader.js
+++ imports/ui/PrivateHeader.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { Accounts } from "meteor/accounts-base";
 import PropTypes from 'prop-types';
 import { createContainer } from 'meteor/react-meteor-data';
+import { Session } from 'meteor/session';
 
 export const PrivateHeader = (props) => {
+  const navImageSrc = props.isNavOpen ? '/images/x.svg' : '/images/bars.svg';
   return (
     <div className="header">
       <div className="header__content">
+        <img onClick={()=> Session.set('isNavOpen', !props.isNavOpen)} src={navImageSrc} />
         <h1 className="header__title">{props.title}</h1>
         <button className="button button--link-text" onClick={() => props.handleLogout()}>Logout</button>
       </div>
@@ -15,12 +18,14 @@ export const PrivateHeader = (props) => {
 }
 PrivateHeader.propTypes = {
   title: PropTypes.string.isRequired,
-  handleLogout: PropTypes.func.isRequired
+  handleLogout: PropTypes.func.isRequired,
+  isNavOpen: PropTypes.bool.isRequired
 }
 
 export default createContainer(() => {
   return {
-    handleLogout: () => Accounts.logout()
+    handleLogout: () => Accounts.logout(),
+    isNavOpen: Session.get('isNavOpen')
   }
 }, PrivateHeader);
 //export default PrivateHeader;
